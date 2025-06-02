cfd63d31b84365763577aae4fb8b14a34aa45495
zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>
2025-06-02 20:41:52 +1000

    feat: implement category api types

--- rest/v1/categories.ts
+++ rest/v1/categories.ts
@@ -1,5 +1,39 @@
-import type {RESTError} from './global';
+import type {Category, Categrories} from '../../payloads';
+import type {RESTError, RESTResponse} from './global';
 
-export interface RESTGetCategoriesError extends RESTError {
+/**
+ * @see {@link https://docs.kick.com/apis/categories#get-categories}
+ */
+export interface RESTGetCategoriesQuery {
+    /**
+     * Search query
+     */
+    q: string;
 
+    /**
+     *  Page number, defaults to `1` if not provided
+     */
+    page?: number;
 }
+
+/**
+ * @see {@link https://docs.kick.com/apis/categories#get-categories}
+ */
+export type RESTGetCategoriesResult = RESTResponse<Categrories>;
+
+/**
+ * @see {@link https://docs.kick.com/apis/categories#get-categories}
+ */
+export type RESTGetCategoriesError = RESTError;
+
+/**
+ * @see {@link https://docs.kick.com/apis/categories#get-categories-category_id}
+ */
+export interface RESTGetCategoryQuery {
+    categoryId: number;
+}
+
+/**
+ * @see {@link https://docs.kick.com/apis/categories#get-categories-category_id}
+ */
+export type RESTGetCategoryResult = RESTResponse<Category>;
--- rest/v1/global.ts
+++ rest/v1/global.ts
@@ -2,3 +2,8 @@ export interface RESTError {
     data: string;
     message: string;
 }
+
+export interface RESTResponse<T> {
+    data: T;
+    message: string;
+}
