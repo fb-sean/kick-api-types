29e7858f4ef7c88e8eaa8b5734cc0f6e5083e7cc
Sean Sattler <sean@sattler.dev>
2025-11-06 18:19:20 +0100

    v1.0.8
    Credits @zSoulweaver

---
+++ payloads/v1/events.ts
@@ -0,0 +1,173 @@
+import type {Emote} from './emote';
+
+/**
+ * @see {@link https://docs.kick.com/events/event-types}
+ */
+export type EventNames =
+    'chat.message.sent' |
+    'channel.followed' |
+    'channel.subscription.renewal' |
+    'channel.subscription.gifts' |
+    'channel.subscription.new' |
+    'livestream.status.updated' |
+    'livestream.metadata.updated' |
+    'moderation.banned';
+
+export interface EventSubscription {
+    app_id: string;
+    broadcaster_user_id: number;
+    created_at: string;
+    event: EventNames;
+    id: string;
+    method: string;
+    updated_at: string;
+    version: number;
+}
+
+export type EventSubscriptions = EventSubscription[];
+
+export interface EventSubscriptionResult {
+    error?: string;
+    name: EventNames;
+    subscription_id?: string;
+    version: number;
+}
+
+export type EventSubscriptionResults = EventSubscriptionResult[];
+
+/**
+ * Represents a user in the context of most of Kick's Event events,
+ * specifically those that include the is_anonymous field.
+ *
+ * @see EventBaseUser
+ */
+export interface EventUser extends EventBaseUser {
+    is_anonymous: boolean;
+}
+
+export interface EventBadge {
+    text: string;
+    type: string;
+    count?: number;
+}
+
+export interface EventBaseUser {
+    user_id?: number;
+    username?: string;
+    is_verified?: boolean;
+    profile_picture?: string;
+    channel_slug?: string;
+}
+
+export interface EventIdentity {
+    username_color: string;
+    badges: EventBadge[];
+}
+
+export interface EventUserWithIdentity extends EventUser {
+    identity: EventIdentity;
+}
+
+/**
+ * Represents a user in the context of a "kicks gifted" Event event. Kick's
+ * API does not provide the is_anonymous field that other Event events have.
+ *
+ * @see EventBaseUser
+ */
+export interface KicksGiftedEventUser extends EventBaseUser {
+}
+
+export interface EventRepliesTo {
+    message_id: string;
+    content: string;
+    sender: EventUser;
+}
+
+export interface ChatMessageEvent {
+    eventType: 'chat.message.sent';
+    eventVersion: '1';
+    message_id: string;
+    replies_to: EventRepliesTo;
+    broadcaster: EventUser;
+    sender: EventUserWithIdentity;
+    content: string;
+    emotes: Emote[];
+    created_at: string;
+}
+
+export interface ChannelFollowEvent {
+    eventType: 'channel.followed';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    follower: EventUser;
+}
+
+export interface SubscriptionRenewalEvent {
+    eventType: 'channel.subscription.renewal';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    subscriber: EventUser;
+    duration: number;
+    created_at: string;
+    expires_at: string;
+}
+
+export interface SubscriptionGiftEvent {
+    eventType: 'channel.subscription.gifts';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    gifter: EventUser;
+    giftees: EventUser[];
+    created_at: string;
+    expires_at: string;
+}
+
+export interface NewSubscriptionEvent {
+    eventType: 'channel.subscription.new';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    subscriber: EventUser;
+    duration: number;
+    created_at: string;
+    expires_at: string;
+}
+
+export interface LivestreamStatusUpdatedEvent {
+    eventType: 'livestream.status.updated';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    is_live: boolean;
+    title: string;
+    started_at: string;
+    ended_at?: string | null;
+}
+
+export interface ModerationBannedEventMetadata {
+    reason: string;
+    created_at: string;
+    expires_at: string;
+}
+
+export interface ModerationBannedEvent {
+    eventType: 'moderation.banned';
+    eventVersion: '1';
+    broadcaster: EventUser;
+    moderator: EventUser;
+    banned_user: EventUser;
+    metadata: ModerationBannedEventMetadata;
+}
+
+export interface KicksGiftedEvent {
+    eventType: 'kicks.gifted';
+    eventVersion: '1';
+    broadcaster: KicksGiftedEventUser;
+    sender: KicksGiftedEventUser;
+    gift: {
+        amount: number;
+        name: string;
+        type: string;
+        tier: string;
+        message: string;
+    };
+    created_at: string;
+}
--- rest/v1/chat.ts
+++ rest/v1/chat.ts
@@ -1,40 +1,40 @@
-import type { RESTResult } from './global'
+import type {RESTResult} from './global';
 
 /**
  * Post a chat message to a channel as a user or a bot. When sending as a user, the broadcaster_user_id is required. Whereas when sending as a bot, the broadcaster_user_id is not required and is ignored. As a bot, the message will always be sent to the channel attached to your token.
- * 
+ *
  * @see {@link https://docs.kick.com/apis/chat#post-chat}
  */
 export interface RESTPostChatMessageBody {
-  /**
-   * The ID of the broadcaster to send the message to.
-   */
-  broadcaster_user_id?: number
+    /**
+     * The ID of the broadcaster to send the message to.
+     */
+    broadcaster_user_id?: number;
 
-  /**
-   * The content of the chat message.
-   * 
-   * Maximum length is 500 characters.
-   */
-  content: string
+    /**
+     * The content of the chat message.
+     *
+     * Maximum length is 500 characters.
+     */
+    content: string;
 
-  /**
-   * The ID of the message to reply to, if applicable.
-   */
-  reply_to_message_id?: string
+    /**
+     * The ID of the message to reply to, if applicable.
+     */
+    reply_to_message_id?: string;
 
-  /**
-   * Specifies whether to send the message as the authorized user or a bot account linked to the Kick application.
-   * 
-   * If set to `'bot'`, the message will be sent to the currently authorized channel.
-   */
-  type: 'user' | 'bot'
+    /**
+     * Specifies whether to send the message as the authorized user or a bot account linked to the Kick application.
+     *
+     * If set to `'bot'`, the message will be sent to the currently authorized channel.
+     */
+    type: 'user' | 'bot';
 }
 
 /**
  * @see {@link https://docs.kick.com/apis/chat#post-chat}
  */
 export type RESTPostChatMessageResult = RESTResult<{
-  is_sent: boolean
-  message_id: string
-}>
+    is_sent: boolean;
+    message_id: string;
+}>;
